dc89c7423ac4c3a693b07ae9a2cae538e69f372e
khoaoe <khoaoe@gmail.com>
2025-11-23 22:32:51 +0700

    add: call vmprint() for init process in exec()

--- kernel/exec.c
+++ kernel/exec.c
@@ -128,6 +128,11 @@ exec(char *path, char **argv)
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
 
+
+  // Print the page table of the first user process (init)
+  if(p->pid == 1)
+    vmprint(p->pagetable);
+
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
